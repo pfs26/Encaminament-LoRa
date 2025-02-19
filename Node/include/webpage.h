9ce95658e03d99a8b6609e18bf296f095eb71624
pfs26 <pflotatss@gmail.com>
2025-02-19 20:56:22 +0100

    Mòdul gestor pàgina web per configuració inicial. Inicia portal capitiu (AP+DNS+WEB). Mètodes per iniciar i aturar

---
+++ Node/include/webpage.h
@@ -0,0 +1,18 @@
+#ifndef _WEBPAGE_H
+#define _WEBPAGE_H
+
+// Nombre màxim de clients que es poden connectar simultaniament. Màxim 10. Millor 1 per evitar desincronitzacions.
+#define MAX_CLIENTS 1   
+// Canal wifi a utilitzar (2.4GHz)
+#define WIFI_CHANNEL 6  
+// Nom de la xarxa que genera el dispositiu
+#define SSID "LORA"     
+// Contrasenya; NULL si és obert; sinó, de més de 8 caràcters en string ()"12345678")
+#define PASSWORD NULL 
+// Interval entre comprovacions de sol·licituds de DNS en ms. 30 sembla ser un valor adequat
+#define DNS_INTERVAL 30 
+
+void webpage_start();
+void webpage_stop();
+
+#endif
