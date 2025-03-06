1f5d2ae6a9b6d23ae5805f8a02cb8b1df2df70e9
pfs26 <pflotatss@gmail.com>
2025-03-06 11:09:20 +0100

    DEV: Millores a traces de debug per mostrar colors més intuitius

--- Node/include/utils.h
+++ Node/include/utils.h
@@ -5,25 +5,30 @@
 #define LOG_LEVEL_WARN  2
 #define LOG_LEVEL_ERROR 3
 
-// Debug macros with log level filtering
+// ANSI color codes
+#define COLOR_RESET  "\033[0m"
+#define COLOR_INFO   "\033[32m"  // Green
+#define COLOR_WARN   "\033[33m"  // Yellow
+#define COLOR_ERROR  "\033[31m"  // Red
+
 #if LOG_LEVEL <= LOG_LEVEL_INFO
-    #define _PI(fmt, ...) Serial.printf("[I]\t%lu\t%s:%d:\t" fmt "\n", millis(), __FILENAME__, __LINE__, ##__VA_ARGS__)
-    // #define _PI(fmt, ...) Serial.printf("[I] " %s:%d: " fmt "\n", __FILENAME__, __LINE__, ##__VA_ARGS__)
+    #define _PI(fmt, ...) Serial.printf(COLOR_INFO "[I]\t%lu\t%s:%d:\t" fmt COLOR_RESET "\n", millis(), __FILENAME__, __LINE__, ##__VA_ARGS__)
 #else
     #define _PI(fmt, ...)
 #endif
 
 #if LOG_LEVEL <= LOG_LEVEL_WARN
-    #define _PW(fmt, ...) Serial.printf("[W]\t%lu\t%s:%d:\t" fmt "\n", millis(), __FILENAME__, __LINE__, ##__VA_ARGS__)
+    #define _PW(fmt, ...) Serial.printf(COLOR_WARN "[W]\t%lu\t%s:%d:\t" fmt COLOR_RESET "\n", millis(), __FILENAME__, __LINE__, ##__VA_ARGS__)
 #else
     #define _PW(fmt, ...)
 #endif
 
 #if LOG_LEVEL <= LOG_LEVEL_ERROR
-    #define _PE(fmt, ...) Serial.printf("[E]\t%lu\t%s:%d:\t" fmt "\n", millis(), __FILENAME__, __LINE__, ##__VA_ARGS__)
+    #define _PE(fmt, ...) Serial.printf(COLOR_ERROR "[E]\t%lu\t%s:%d:\t" fmt COLOR_RESET "\n", millis(), __FILENAME__, __LINE__, ##__VA_ARGS__)
 #else
     #define _PE(fmt, ...)
 #endif
+
 
 #define MAX(a,b) ((a) > (b) ? (a) : (b))
 #define MIN(a,b) ((a) < (b) ? (a) : (b))
