f7b2c704b4bc17a16741b327dc36889361f88bcf
pfs26 <pflotatss@gmail.com>
2025-03-07 11:50:50 +0100

    chore: reorganitzar fitxers no de capa, però d'utilitat

--- Node/src/utils/RingBuffer.cpp
+++ Node/src/utils/RingBuffer.cpp
@@ -1,3 +1,10 @@
+/*
+    Implementació d'un buffer circular de valors de 16 bits.
+        1. Si s'afegeix un valor i el buffer està ple, es substitueix pel valor més antic
+        2. Permet afegir i eliminar valors del buffer
+        3. Permet buscar elements dins del buffer
+*/
+
 #include "RingBuffer.h"
 
 RingBuffer::RingBuffer(int size) {
@@ -16,14 +23,14 @@ void RingBuffer::enqueue(uint16_t value) {
     buffer[head] = value;
     head = (head + 1) % capacity;
     if (count == capacity) {
-        tail = (tail + 1) % capacity;  // Overwrite oldest
+        tail = (tail + 1) % capacity;  // Sobreescriure més antic (cua)
     } else {
         count++;
     }
 }
 
 uint16_t RingBuffer::dequeue() {
-    if (count == 0) return 255; // Return invalid if empty
+    if (count == 0) return 255; // Valor arbitrari si és buida
     uint16_t value = buffer[tail];
     tail = (tail + 1) % capacity;
     count--;
@@ -43,8 +50,8 @@ bool RingBuffer::remove(uint16_t value) {
     int shiftCount = 0;
     for (int i = 0, idx = tail; i < count; i++, idx = (idx + 1) % capacity) {
         if (buffer[idx] == value) {
-            shiftCount++;  // Mark this value to be removed
-        } else if (shiftCount > 0) {
+            shiftCount++;  // S'ha trbat l'element a eliminar
+        } else if (shiftCount > 0) { // Un cop trobat, desplaçar elements següents
             // Shift elements forward
             int prevIdx = (idx - shiftCount + capacity) % capacity;
             buffer[prevIdx] = buffer[idx];
@@ -52,7 +59,7 @@ bool RingBuffer::remove(uint16_t value) {
     }
 
     count -= shiftCount;
-    head = (tail + count) % capacity; // Adjust head position
+    head = (tail + count) % capacity;
     return shiftCount > 0;
 }
 
