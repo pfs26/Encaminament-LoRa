d80f1d0ea57e7489efb68a9222b79c0e9fe9d51e
pfs26 <pflotatss@gmail.com>
2025-03-21 22:43:53 +0100

    feat: implementari lògica routing per enviar cap a lorawan si som gateway

--- Node/src/routing.cpp
+++ Node/src/routing.cpp
@@ -15,11 +15,16 @@ static std::vector<mac_id_t> higherLayerPackets;
 
 void _printPacket(const routing_pdu_t* const pdu);
 void _onMacReceived(void);
-void _onMacSend(mac_id_t);
-void _onMacTxFailed(mac_id_t);
+void _onMacSend(uint16_t);
+void _onMacTxFailed(uint16_t);
+void _onWANReceived(void);
+void _processReceivedPacket(size_t length);
 void _packetReceived();
-void _packetSent();
-
+void _packetSent(uint16_t id);
+void _packetTxFailed(uint16_t id);
+routing_err_t _sendThroughLoRaWAN(routing_pdu_t* pdu, uint16_t* id=nullptr);
+void _WANPacketSent();
+void _WANPacketTxFailed();
 
 bool Routing_init(node_address_t selfAddr, bool is_gateway) {
     // Inicialitzar capa MAC
@@ -37,6 +42,11 @@ bool Routing_init(node_address_t selfAddr, bool is_gateway) {
         return false;
     }
 
+    if(isGateway && !LW_init()) {
+        _PE("[ROUTING] Error initializing LoRaWAN");
+        return false;
+    }
+
     if(!RoutingTable_init()) {
         _PE("[ROUTING] Error initializing routing table");
         return false;
@@ -49,6 +59,8 @@ bool Routing_init(node_address_t selfAddr, bool is_gateway) {
     MAC_onSend(_onMacSend);
     MAC_onTxFailed(_onMacTxFailed);
 
+    LW_onReceive(_onWANReceived);
+
     _PI("[ROUTING] Initialized");
     return true;
 }
@@ -58,25 +70,15 @@ void Routing_deinit() {
     onPacketSent = onTxError = nullptr;
     higherLayerPackets.clear();
     RoutingTable_deinit();
+    LW_deinit();
     MAC_deinit();
     _PI("[ROUTING] Deinitialized");
 }
 
 routing_err_t Routing_send(node_address_t dst, const routing_data_t data, size_t length, uint16_t* id) {
-    // Comprovar si hi ha ruta a la taula
-    // Si no hi ha ruta, enviar paquet de descobriment
-    // Si hi ha ruta, enviar paquet amb la ruta
-    // Si no es pot enviar, retornar error
-
     if(length > ROUTING_MAX_DATA_SIZE) {
         _PW("[ROUTING] Data too long (%d)", length);
         return ROUTING_ERR_MAX_LENGTH;
-    }
-
-    node_address_t nextHop = RoutingTable_getRoute(dst);
-    if(nextHop == 0x00) {
-        _PW("[ROUTING] No route to 0x%02X", dst);
-        return ROUTING_ERR_NO_ROUTE;
     }
 
     txPDU.src = self;
@@ -87,16 +89,34 @@ routing_err_t Routing_send(node_address_t dst, const routing_data_t data, size_t
 
     _PI("[ROUTING] Sending packet:");
     _printPacket(&txPDU);
-    // @todo: falta filtrar si som gateway, enviar a través de wan. Potser ho ha de fer MAC
-    // a partir de l'adreça de destí?
-    mac_id_t packetID;
-    mac_err_t err = MAC_send(nextHop, (uint8_t*)&txPDU, length+ROUTING_HEADERS_SIZE, &packetID);
 
-    if(err != MAC_SUCCESS) {
-        _PW("[ROUTING] Error sending PDU (state: %d)", err);
+    uint16_t packetID;
+    routing_err_t state = ROUTING_ERR;
+    // Si som gateway i destí és gateway, enviar a través de lorawan
+    // Només hauria de passar si capa superior vol enviar alguna cosa?
+    if (isGateway && dst == NODE_GATEWAY_ADDRESS) {
+        state = _sendThroughLoRaWAN(&txPDU, &packetID);
+    }
+    else {
+        // En altres casos, el paquet és per la mateixa xarxa, i s'envia a través de MAC
+        node_address_t nextHop = RoutingTable_getRoute(dst);
+        if(nextHop == 0x00) {
+            _PW("[ROUTING] No route to 0x%02X", dst);
+            return ROUTING_ERR_NO_ROUTE;
+        }
+
+        mac_err_t err = MAC_send(nextHop, (uint8_t*)&txPDU, length+ROUTING_HEADERS_SIZE, &packetID);
+        state = err == MAC_SUCCESS ? ROUTING_SUCCESS : ROUTING_ERR;
+    }
+
+    // Filtrem errors de TX
+    if(state == ROUTING_ERR) {
+        _PW("[ROUTING] Error sending PDU");
         return ROUTING_ERR;
     }
 
+    // Si s'ha pogut enviar, afegir a llista de paquets que cal notificar a capa superior
+    // És responabilitat de capa superior guardar-se ID per si vol actuar sobre aquest paquet i event
     higherLayerPackets.push_back(packetID);
     if (id)
         *id = packetID;
@@ -118,13 +138,25 @@ void Routing_onReceive(routing_rx_callback_t cb) { onPacketReceived = cb; }
 void Routing_onSend(routing_tx_callback_t cb) { onPacketSent = cb; }
 void Routing_onTxError(routing_tx_callback_t cb) { onTxError = cb; }
 
-void _onMacReceived(void) {
-    // Executat quan MAC obté un frame per nosaltres; cal que processem el paquet
-    // i veure si és per nosaltres o cal reenviar-lo
-    size_t MAClength = 0;
-    // Podem copiar directament sobre PDU, ja es farà el mapeig correcte
-    node_address_t tx = MAC_receive((mac_data_t*)&rxPDU, &MAClength);
+routing_err_t _sendThroughLoRaWAN(routing_pdu_t* pdu, uint16_t* id) {
+    _PI("[ROUTING] Sending packet to gateway. Forwarding to LoRaWAN");
+    bool state = LW_send((uint8_t*)pdu, pdu->dataLength+ROUTING_HEADERS_SIZE);
+    if (id) 
+        *id = 0; // ID = 0 no es pot generar mai a capa MAC, per tant és un valor segur per identificar que és un paquet LoRaWAN
+                 // Tampoc s'hauria de donar el cas que es vulguin fer dues transmissions WAN simultànies   
+    // Programem execució d'esdeveniments simulats per mantenir coherència amb capa MAC
+    if (state) {
+        scheduler_once(_WANPacketSent);
+        return ROUTING_SUCCESS;
+    }
+    else {
+        scheduler_once(_WANPacketTxFailed);
+        return ROUTING_ERR;
+    }
+}
 
+void _processReceivedPacket(size_t length) {
+    // Si destí de paquet som nosalters, notifiquem capa superior
     if(rxPDU.dst == self) {
         _PI("[ROUTING] Received packet from 0x%02X", rxPDU.src);
         _printPacket(&rxPDU);
@@ -132,55 +164,78 @@ void _onMacReceived(void) {
         return;
     }
 
-    // @todo: Falta filtrar per si som gateway -> cal reenviar-ho a través de MAC cap a gateway!
-    //        important que gateway estigui a taula de rutes correctament definit!
-
+    // si TTL és 1 significa que s'havia enviat amb TTL = 1 i, per tant, que únicament ha de fer un salt
+    // Si filtressim per ttl == 0 en faria 2, ja que el primer salt el decrementa a 1
     if(rxPDU.ttl == 1) {
         _PW("[ROUTING] TTL expired");
-        return;
-    }
-
-    node_address_t nextHop = RoutingTable_getRoute(rxPDU.dst);
-    if(nextHop == NODE_ADDRESS_NULL) {
-        _PW("[ROUTING] No route to 0x%02X", rxPDU.dst);
         return;
     }
 
     // Actualitzar TTL
     rxPDU.ttl--;
 
+    // Si som gateway i destí és gateway, enviar a través de lorawan
+    // Passem a capa superior per si cal gestionar ACK!
+    // @todo: OJU QUE SI ES MANTÉ AIXÍ NO PASSARÀ MAI PERQUE ABANS ES DONARÀ AL CONDICIÓ DE LÍNIA 149!!!!
+    if (isGateway && rxPDU.dst == NODE_GATEWAY_ADDRESS) {
+        _PI("[ROUTING] Received packet for gateway. Forwarding to LoRaWAN");
+        _sendThroughLoRaWAN(&rxPDU);
+        _packetReceived();
+        return;
+    }
+
+    // Si no som gateway, o destí no és gateway, enviar a través de MAC (lora raw)
+    // obtenint següent hop de taula de rutes
+    node_address_t nextHop = RoutingTable_getRoute(rxPDU.dst);
+    if(nextHop == NODE_ADDRESS_NULL) {
+        _PW("[ROUTING] No route to 0x%02X", rxPDU.dst);
+        return;
+    }
+
     // Reenviem amb MAC_send, i ens despreocupem de si s'acaba enviant o no; MAC ja ho intentarà gestionar tant bé com pugui (reintents, BEB, etc.)
-    mac_err_t err = MAC_send(nextHop, (uint8_t*)&rxPDU, MAClength); // MAClength no hauria de canviar si únicament es canvia TTL
+    mac_err_t err = MAC_send(nextHop, (uint8_t*)&rxPDU, length); // MAClength no hauria de canviar si únicament es canvia TTL
     _PI("[ROUTING] Forwarded packet to 0x%02X", rxPDU.dst);
 }
 
-void _onMacSend(mac_id_t id) {
-    // Probablement no utilitzat
-    if(onPacketSent == nullptr) {
+void _onWANReceived(void) {
+    // Executat quan hi ha una nova recepció de dades a través de LoRaWAN
+    // Cal processar-ho i actuar com si fos una recepció de MAC
+    size_t length;
+    uint8_t port;
+    if(!LW_receive((uint8_t*)&rxPDU, &length, &port)) {
+        _PW("[ROUTING] No data received from LoRaWAN");
         return;
     }
+    _processReceivedPacket(length);
+}
+
+void _onMacReceived(void) {
+    // Executat quan MAC obté un frame per nosaltres; cal que processem el paquet
+    // i veure si és per nosaltres o cal reenviar-lo
+    size_t MAClength = 0;
+    // Podem copiar directament sobre PDU, ja es farà el mapeig correcte
+    node_address_t tx = MAC_receive((mac_data_t*)&rxPDU, &MAClength);
+    _processReceivedPacket(MAClength);
+}
+
+void _onMacSend(uint16_t id) {
     int pos = 0; // Per guardar quin element s'ha d'eliminar
     for(int value : higherLayerPackets) { // iterar per cada element del vector
         if(value == id) {
             higherLayerPackets.erase(higherLayerPackets.begin() + pos);
-            onPacketSent(id);
+            _packetSent(id);
             return;
         }
         pos++;
     }
 }
 
-void _onMacTxFailed(mac_id_t id) {
-    // Probablement no utilitzat; routing només es preocupa d'obtenir següent RX i enviar,
-    // MAC és qui gestiona reintents
-    if(onTxError == nullptr) {
-        return;
-    }
+void _onMacTxFailed(uint16_t id) {
     int pos = 0; // Per guardar quin element s'ha d'eliminar
     for(int value : higherLayerPackets) { // iterar per cada element del vector
         if(value == id) {
             higherLayerPackets.erase(higherLayerPackets.begin() + pos);
-            onTxError(id);
+            _packetTxFailed(id);
             return;
         }
         pos++;
@@ -191,6 +246,29 @@ void _packetReceived() {
     if(onPacketReceived != nullptr) {
         onPacketReceived();
     }
+}
+
+void _packetSent(uint16_t id) {
+    if(onPacketSent != nullptr) {
+        onPacketSent(id);
+    }
+}
+
+void _packetTxFailed(uint16_t id) {
+    if(onTxError != nullptr) {
+        onTxError(id);
+    }
+}
+
+void _WANPacketSent() {
+    // Executat quan s'ha enviat un paquet a través de LoRaWAN
+    // Esdeveniments simulats per mantenir coherència amb capa MAC, a través de scheduler
+    _packetSent(0);
+}
+
+void _WANPacketTxFailed() {
+    // Executat quan s'ha intentat enviar un paquet a través de LoRaWAN i ha fallat
+    _packetTxFailed(0);
 }
 
 #if LOG_LEVEL <= LOG_LEVEL_INFO
@@ -207,4 +285,3 @@ void _printPacket(const routing_pdu_t* const pdu) {
 #else
 void _printPacket(const routing_pdu_t* const pdu) {}
 #endif
-
