7f081ccd789ea5c13d63de30d0d1f83402086c4a
pfs26 <pflotatss@gmail.com>
2025-03-21 23:20:30 +0100

    readme update

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,48 +1,39 @@
+/*
+    Exemple amb dos nodes: un és transmissor i l'altre receptor.
+    El transmissor envia frames cada 5 segons. El retard del tansmissor és bloquejant (fet amb delay())
+    El receptor imprimeix les dades rebudes per pantalla, i envia ACK si li toca
+*/
+
 #include <Arduino.h>
-#include "mac.h"
+#include "transport.h"
+#include "routing_table.h"
 #include "scheduler.h"
 #include "utils.h"
 
-// #define SENDER1
+#define SENDER
 
-void Send() {
-    #ifdef SENDER1
-        mac_data_t data = "Hola 0x02!";
-        mac_addr_t rx = 0x02;
-    #else
-        mac_data_t data = "Hola 0x01!";
-        mac_addr_t rx = 0x01;
-    #endif
-    while(MAC_send(rx, data, 10) != MAC_ERR_SUCCESS);
-}
+int count = 0;
 
 void onSend() {
-    Serial.println("MAC frame sent");
-    #ifdef SENDER1
-    scheduler_once(Send, 2500);
-    #else
-    scheduler_once(Send, 10000);
-    #endif
-}
-
-void onErr() {
-    Serial.println("Error sending mac frame");
-    #ifdef SENDER1
-    scheduler_once(Send, 2500);
-    #else
-    scheduler_once(Send, 10000);
-    #endif
+    Serial.println("Segment sent");
+    transport_data_t data = "UDP!!";
+    transport_err_t state = Transport_send(0x02, 0x01, data, 5, false);
+    if(state != TRANSPORT_SUCCESS) {
+        Serial.printf("Error sending: %d\n", state);
+        return;
+    }
+    Serial.println("Segment scheduled to be sent");
 }
 
 void onRcv() {
-    Serial.println("MAC frame received");
-    mac_data_t data;
-    uint8_t length;
-    mac_addr_t tx = MAC_receive(&data, &length);
-    Serial.printf("\tTX: %d\n\tData: %s\tLength: %d\n", tx, data, length);
+    Serial.println("Segment received");
+    transport_data_t data;
+    size_t length;
+    uint8_t port;
+    node_address_t tx = Transport_receive(&port, &data, &length);
+    data[length] = '\0'; // Per poder imprimir amb serial
+    Serial.printf("\tTX: %d\tData: %s\tLength: %d\tPort: %d\n", tx, data, length, port);
 }
-
-
 
 void setup() {
     Serial.begin(115200);
@@ -54,94 +45,41 @@ void setup() {
     Serial.println(esp_reset_reason());
 
 
-    #ifdef SENDER1
-    mac_addr_t addr = 0x01;
+    #ifdef SENDER
+    node_address_t addr = 0x01;
     #else
-    mac_addr_t addr = 0x02;
+    node_address_t addr = 0x02;
     #endif
-    if(!MAC_init(addr, false)) {
-        _PL("ERR");
+    if(!Transport_init(addr, false)) {
+        _PE("ERR");
         while(1);
     }
 
-    MAC_onSend(onSend);
-    MAC_onTxFailed(onErr);
-    MAC_onReceive(onRcv);
 
-    scheduler_once(Send);
+    Transport_onReceive(onRcv);
+    Transport_onSend(onSend);
+
+    RoutingTable_clear();
+    #ifdef SENDER
+        RoutingTable_addRoute(0x02, 0x02);
+        transport_data_t data = "Hola!";
+        transport_err_t state = Transport_send(0x02, 0x01, data, 5, true);
+        if(state != TRANSPORT_SUCCESS) {
+            Serial.printf("Error sending: %d\n", state);
+            return;
+        }
+        Serial.println("Segment scheduled to be sent");
+        state = Transport_send(0x02, 0x01, data, 5, true);
+        if(state != TRANSPORT_SUCCESS) {
+            Serial.printf("Error sending: %d\n", state);
+            return;
+        }
+        Serial.println("Segment scheduled to be sent");
+    #else
+        RoutingTable_addRoute(0x01, 0x01);
+    #endif
 }
 
 void loop() {
     scheduler_run();
 }
-
-
-// /*
-//     Exemple 
-// */
-
-
-// #include <Arduino.h>
-// #include "mac.h"
-// #include "scheduler.h"
-// #include "utils.h"
-
-// // #define SENDER
-
-// int count = 0;
-
-// void onSend() {
-//     Serial.println("MAC frame sent");
-//     delay(5000);
-//     mac_data_t data = "Hola!";
-//     MAC_send(0x02, data, 5);
-// }
-
-// void onErr() {
-//     Serial.println("Error sending mac frame");
-//     delay(5000);
-//     mac_data_t data = "Hola!";
-//     MAC_send(0x02, data, 5);
-// }
-
-// void onRcv() {
-//     Serial.println("MAC frame received");
-//     mac_data_t data;
-//     uint8_t length;
-//     mac_addr_t tx = MAC_receive(&data, &length);
-//     Serial.printf("\tTX: %d\n\tData: %s\tLength: %d\n", tx, data, length);
-// }
-
-// void setup() {
-//     Serial.begin(115200);
-
-//     Serial.print(F("[SX1262] Initializing ... "));
-//     Serial.print("Model: "); Serial.println(ESP.getChipModel());
-//     Serial.print("CPU: "); Serial.println(ESP.getCpuFreqMHz());
-//     Serial.print("Heap: "); Serial.println(ESP.getFreeHeap());
-//     Serial.println(esp_reset_reason());
-
-
-//     #ifdef SENDER
-//     mac_addr_t addr = 0x01;
-//     #else
-//     mac_addr_t addr = 0x02;
-//     #endif
-//     if(!MAC_init(addr, false)) {
-//         _PL("ERR");
-//         while(1);
-//     }
-
-//     MAC_onSend(onSend);
-//     MAC_onTxFailed(onErr);
-//     MAC_onReceive(onRcv);
-
-//     #ifdef SENDER
-//         mac_data_t data = "Hola!";
-//         MAC_send(0x02, data, 5);
-//     #endif
-// }
-
-// void loop() {
-//     scheduler_run();
-// }
